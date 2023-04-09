ef0a3c5949bf76c7b80e4cdd00ca44b75aa41226
surazz999 <surazsub999@gmail.com>
2023-04-09 16:25:20 +0545

    transaction list could be added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,50 @@
+import 'package:flutter_complete_guide/widgets/user_transaction.dart';
+
+import './widgets/new_transaction.dart';
+
+import './widgets/transactions_list.dart';
+
 import 'package:flutter/material.dart';
 
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          UserTransaction()
+        ],
+      ),
+    );
   }
 }
